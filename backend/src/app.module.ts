d9de0cb3fd81cc18ee7379e191418ee5eeaf62a9
lechuan011 <lechuan2001@gmail.com>
2023-12-16 21:03:31 +0700

    Location CRUD API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { LocationModule } from './location/location.module';
 import { ReviewModule } from './review/review.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -15,7 +15,8 @@ import { AuthModule } from './auth/auth.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_URI'), // Loaded from .ENV
+        uri: config.get<string>('MONGO_URI'), 
+        dbName: config.get<string>('DATABASE_NAME'), // Đặt tên cơ sở dữ liệu ở đây
       }),
     }),
     ConfigModule.forRoot({ isGlobal: true }),
@@ -25,6 +26,15 @@ import { AuthModule } from './auth/auth.module';
     UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    },
+    AppService],
 })
 export class AppModule {}
--- backend/src/location/dto/update-location.dto.ts
+++ backend/src/location/dto/update-location.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLocationDto } from './create-location.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, IsArray } from 'class-validator';
 
-export class UpdateLocationDto extends PartialType(CreateLocationDto) {}
+export class UpdateLocationDto {
+  @ApiProperty({
+    example: 'Tòa nhà H6',
+    description: 'The name of the location',
+  })
+  @IsString()
+  @IsOptional()
+  readonly name?: string;
+
+  @ApiProperty({
+    example: 'Tòa nhà H6 - Đại học Bách Khoa - ĐHQG TP.HCM gồm nhiều phòng học và phòng lab hiện đại',
+    description: 'The information about the location',
+  })
+  @IsString()
+  @IsOptional()
+  readonly introduction?: string;
+
+  @ApiProperty({
+    example: ['url1', 'url2'],
+    description: 'URLs of images associated with the location',
+  })
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  readonly imagesUrls?: string[];
+}
