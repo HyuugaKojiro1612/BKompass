21ca8b61517fa3511b0a1e1081ff2259338a328b
HyuugaKojiro1612 <minhquannguyenhue@gmail.com>
2023-12-16 15:01:12 +0700

    Backend: Rearrange template

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { LocationModule } from './location/location.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [UserModule, LocationModule],
+  imports: [UserModule, LocationModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
