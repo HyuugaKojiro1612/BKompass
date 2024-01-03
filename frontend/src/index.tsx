34210ea0c2fe29109f4cf3d11eba3c5732b61bcc
HyuugaKojiro1612 <minhquannguyenhue@gmail.com>
2024-01-03 19:11:09 +0700

    minor: ignore warning in expo

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,6 +6,9 @@ import { store, persistor } from "@/Store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { ApplicationNavigator } from "./Navigation";
+import { LogBox } from "react-native"
+
+LogBox.ignoreAllLogs(true)
 
 i18n.locale = Localization.locale;
 i18n.enableFallback = true;
